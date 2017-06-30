862b964e10fa9a0543d5a48d88a7c11e8457c125
Yuan Tao <towyuan@outlook.com>
2017-06-30 17:53:29 +0800

    Refactor

--- examples/pack.js
+++ examples/pack.js
@@ -7,7 +7,6 @@ const packer = new Packer(__dirname);
 (async() => {
 	try {
 		await packer.collect();
-		packer.genhooks();
 		const output = await packer.pack();
 		console.log(output);
 	} catch (e) {
